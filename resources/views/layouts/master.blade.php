5476435db212a9e559f7a0bde0ca29020ad75dbe
img21326 <img21326@gmail.com>
2017-10-23 12:57:30 +0800

    編輯 resources/views/layouts資料夾內的 master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,8 @@
         <title>@yield('title')|MyBlog</title>
     </head>
     <body>
-        <divclass="container">
+        @include('layouts.partials.navigation')
+        <div class="container">
             @yield('content')
         </div>
     </body>
